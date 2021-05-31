b9868001e8bc40bbc3ff11986254689304dafc2a
donglu <donglu@starot.com>
2021-05-31 11:54:34 +0800

    解析完成产生式

--- 4 syntax analysis/firstFollow.py
+++ 4 syntax analysis/firstFollow.py
@@ -6,53 +6,83 @@ class ParseError(Exception):
 
 
 class Parser:
-    def __init__(self, ):
-        self.lookahead = sys.stdin.read(1)
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.length = len(self.buffer)
+        self.next = 0
 
-    def expr(self):
-        self.term()
-        while True:
-            if self.lookahead == '+':
-                self.match('+')
-                # 这里为自己修改后支持*
-                self.expr()
-                sys.stdout.write('+')
-            elif self.lookahead == '-':
-                self.match('-')
-                # 这里为自己修改后支持*
-                self.expr()
-                sys.stdout.write('-')
-            elif self.lookahead == '*':
-                self.match('*')
-                self.term()
-                sys.stdout.write('*')
+    def production(self):
+        left = self.nonterminal()
+        self.match_space()
+        self.match('-')
+        if self.next >= self.length:
+            raise ParseError('syntax error')
+        self.match('>')
+        sysmbols = []
+        productions = []
+        self.match_space()
+        while self.next < self.length:
+            right = self.sysmbol()
+            if right == '|':
+                productions.append((left, sysmbols))
+                sysmbols = []
             else:
-                return
+                sysmbols.append(right)
+            if self.next >= self.length:
+                break
+            self.match_space()
+        productions.append((left, sysmbols))
+        return productions
 
-    def term(self):
-        if self.lookahead.isdigit():
-            sys.stdout.write(self.lookahead)
-            self.match(self.lookahead)
-        else:
-            raise ParseSyntaxError('syntax error')
+    def nonterminal(self):
+        start = self.next
+        if not self.buffer[self.next].isalpha():
+            raise ParseError('syntax error')
+        self.next += 1
+        while self.next < self.length:
+            if self.buffer[self.next].isspace():
+                break
+            else:
+                self.next += 1
+        return self.buffer[start:self.next]
+
+    def sysmbol(self):
+        start = self.next
+        while self.next < self.length:
+            if self.buffer[self.next].isspace():
+                break
+            else:
+                self.next += 1
+        return self.buffer[start:self.next]
 
     def match(self, t):
-        if self.lookahead == t:
-            self.lookahead = sys.stdin.read(1)
+        if self.buffer[self.next] == t:
+            self.next += 1
         else:
-            raise ParseSyntaxError('syntax error')
+            raise ParseError('syntax error')
+
+    def match_space(self):
+        while self.next < self.length:
+            if self.buffer[self.next].isspace():
+                self.next += 1
+            else:
+                break
 
 
 def parse_line(line):
     line = line.strip()
-    print(line)
-    for
+    p = Parser(line)
+    return p.production()
+
 
 def parse_file(file_path):
     rules = []
     with open(file_path) as f:
         for line in f:
-            parse_line(line)
+            rules.extend(parse_line(line))
+    print(rules)
+    for s, l in rules:
+        print(f'{s} -> {" ".join(l)}')
 
 
 if __name__ == '__main__':
